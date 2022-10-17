2038eb9ceea049f80b87c0e40b0cffce176eac7e
mooncoded <mooncoded.bs@gmail.com>
2022-10-17 16:40:28 -0300

    google tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,28 +37,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           ></link>
           <link rel="shortcut icon" href="static/favicon.ico" />
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WMNL9WJ');`,
-            }}
-          >
-            {" "}
-          </Script>
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WMNL9WJ"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
           <script>0</script>
           <Main />
           <NextScript />
