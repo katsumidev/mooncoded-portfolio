7101778a6001dc6d6a899cc8175772e9630da69d
mooncoded <mooncoded.bs@gmail.com>
2022-10-17 17:35:16 -0300

    sitemap

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   // eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
   swcMinify: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
