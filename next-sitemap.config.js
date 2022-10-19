2ddc4e2081bda09f092eaef2ff326e51878ba797
mooncoded <mooncoded.bs@gmail.com>
2022-10-18 22:44:09 -0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://mooncoded.vercel.app',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+  siteUrl: process.env.SITE_URL || "https://mooncoded.vercel.app",
+};
